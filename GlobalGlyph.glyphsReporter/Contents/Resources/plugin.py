bda2d50a31620687a61ba21db850e0c9687866b7
Jens Kutilek <webmail@kutilek.de>
2026-07-30 13:23:57 +0200

    Remove Python 2 stuff

--- GlobalGlyph.glyphsReporter/Contents/Resources/plugin.py
+++ GlobalGlyph.glyphsReporter/Contents/Resources/plugin.py
@@ -1,6 +1,3 @@
-# encoding: utf-8
-from __future__ import division, print_function, unicode_literals
-
 import traceback
 
 ###########################################################################################################
